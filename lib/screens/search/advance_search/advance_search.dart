14de032786130eea1d6a374383baef8220c4a14f
Tdt <tta424517@gmail.com>
2024-04-04 17:20:13 +0700

    Update

---
+++ lib/screens/search/advance_search/advance_search.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AdvanceSearch extends StatefulWidget {
+  const AdvanceSearch({Key? key}) : super(key: key);
+
+  @override
+  _AdvanceSearchState createState() => _AdvanceSearchState();
+}
+
+class _AdvanceSearchState extends State<AdvanceSearch> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
